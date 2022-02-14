d16e5c6895dff4c42c98219fda209e3353d4ed5e
KGOH <kgofhedgehogs@gmail.com>
2022-02-14 14:32:34 +0200

    Add touch stone client base url aud
    Co-authored-by: @ddegr <de@health-samurai.io>

--- scripts/smart-on-fhir-setup.sh
+++ scripts/smart-on-fhir-setup.sh
@@ -12,6 +12,11 @@ curl -o /dev/null -u ${AIDBOX_CLIENT_ID}:${AIDBOX_CLIENT_SECRET} \
      --data-raw '{"source": "https://storage.googleapis.com/aidbox-public/inferno/inferno-community-fixtures.ndjson.gz"}'
 
 curl -o /dev/null -u ${AIDBOX_CLIENT_ID}:${AIDBOX_CLIENT_SECRET} \
+     -X PUT 'http://localhost:8888/Client' \
+     -H 'Content-Type: application/json' \
+     --data-raw '{ "auth" : { "authorization_code" : { "audience" : [ "${AIDBOX_BASE_URL}/smart" ], "redirect_uri" : "https://touchstone.aegis.net/touchstone/oauth2/authcode/redirect", "refresh_token" : true, "access_token_expiration" : 300 } }, "secret" : "touchstone-secret", "grant_types" : [ "code", "basic" ], "id" : "touchstone-client", "resourceType" : "Client" }'
+
+curl -o /dev/null -u ${AIDBOX_CLIENT_ID}:${AIDBOX_CLIENT_SECRET} \
      -X PUT 'http://localhost:8888/' \
      -H 'Content-Type: text/yaml' \
      --data-binary "@./scripts/smart-on-fhir-resources/client.yaml"
